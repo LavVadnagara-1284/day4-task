d6b66dc9dbf15c5ea2c35f5e437e46c6ae65a7b6
LavVadnagara1284 <lav.vadnagara@adit.com>
2025-03-17 15:34:02 +0530

    second commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // Create User
+  @Post()
+  create(@Body() user: UserDto) {
+    return this.usersService.createUser(user);
+  }
+
+  // Get All Users
+  @Get()
+  findAll() {
+    return this.usersService.findAllUsers();
+  }
+
+  // Update User by ID
+  @Put(':id')
+  update(@Param('id') id: string, @Body() user: UserDto) {
+    return this.usersService.updateUser(id, user);
+  }
+
+  // Delete User by ID
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.removeUser(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: UserDto[] = [];
+
+  createUser(user: UserDto) {
+    user.id = Date.now().toString();
+    this.users.push(user); // this will push the new user into the users array.
+    return {
+      message: 'User created successfully',
+      data: user,
+    };
+  }
+
+  findAllUsers() {
+    return {
+      message: 'All users retrieved successfully',
+      data: this.users, // this will return the all users from the users array
+    };
+  }
+
+  updateUser(id: string, updateUserDto: UserDto) {
+    const index = this.users.findIndex((user) => user.id === id); // this will return the user with the specific index
+    if (index === -1) return null; // this will return null if the user doesn't exist
+
+    this.users[index] = { ...this.users[index], ...updateUserDto }; // this will push the updated user into the users array for specific index
+    return {
+      message: 'User updated successfully',
+      data: this.users[index], // this will return the updated user from the users array for specific index
+    };
+  }
+
+  removeUser(id: string) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) return null;
+
+    return {
+      message: 'User deleted successfully',
+      data: this.users.splice(index, 1)[0],
+    }; // this will remove the user from the array and returns the deleted user.
+  }
+}
